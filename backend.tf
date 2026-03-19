447d0334778b0d093ca0989ed4abfb8d1d61a992
jaezeu <jazeel.meerasah@gmail.com>
2026-03-19 21:37:44 +0800

    fmt

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
-    backend "s3" {
-        bucket = "sctp-ce12-tfstate-bucket"
-        key    = "terraform-cichecks.tfstate"
-        region = "ap-southeast-1"
-    }
+  backend "s3" {
+    bucket = "sctp-ce12-tfstate-bucket"
+    key    = "terraform-cichecks.tfstate"
+    region = "ap-southeast-1"
+  }
 }
